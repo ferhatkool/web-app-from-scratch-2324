607d864b5862900659c1a498d2d92e62728683e3
ferhatkool <ferhat.kool@hva.nl>
2024-02-09 20:13:52 +0100

    Changed the buttons, so they open a dialog instead of using javascript. Added top 5 lists for Heroes and Villains. Added more data to JSON. Added context to about me dialog.

--- docs/js/buttonClick.js
+++ docs/js/buttonClick.js
@@ -1,34 +1,39 @@
-// Dit script wordt gebruikt om de knoppen op de 'homepage' bruikbaar te maken.
-// Toevoegen van een listener voor het detecteren van een ingelaadde pagina.
-document.addEventListener('DOMContentLoaded', function() {
-    // Selecteer alle elementen die de class 'toggle' bevatten, wat alle vijf de knoppen zijn.
-    const toggle = document.querySelectorAll('.toggle');
-    // Selecteer de DIV dat wordt weergegeven bij het bezoeken van de site.
-    const defaultDiv = document.querySelector('.default');
-    // Voor elk element (button), voer iets uit.
-    toggle.forEach(button => {
-        // Luister naar clicks op de elementen (buttons)
-        button.addEventListener('click', function() {
-            // Stel een zoek waarde in, wat 'data-target' is.
-            const targetId = this.getAttribute('data-target');
-            // Zoek naar elementen die het ID van de voorgaande waarde hebben.
-            const targetContent = document.getElementById(targetId);
-            // Selecteer elk element (div) dat de class 'content' bevat en doe iets voor elk element.
-            document.querySelectorAll('.content').forEach(content => {
-                // Controleer of de display waarde van content op alles behalve none staat. Zodra dit niet gelijk is, pas de display style aan naar none.
-                if (content !== targetContent) {
-                    content.style.display = 'none';
-                }
-            });
-            // Controleer of de display van targetContent op alleen none staat. Zodra dit zo is, zet deze display op flex en de standaard weergegeven DIV op none. HIerdoor wordt er een nieuwe 'pagina' geopend op de one-pager.
-            if (targetContent.style.display === 'none') {
-                targetContent.style.display = 'flex';
-                defaultDiv.style.display = 'none';
-            // Mocht er niet aan de bovenstaande eis worden voldaan, verander dan de display op none en de standaard weergegeven DIV op flex. Hierdoor wordt er oorspronkelijke tekst weer naar voren.
-            } else {
-                targetContent.style.display = 'none';
-                defaultDiv.style.display = 'flex';
-            }
-        });
-    });
-});
+const dialogAboutMeButton = document.getElementById('aboutMeButton');
+const dialogAboutMe = document.getElementById('dialogAboutMe');
+const dialogAboutMeDiv = document.getElementById('aboutMeStopPropagation');
+
+dialogAboutMeButton.addEventListener('click', () => dialogAboutMe.showModal());
+dialogAboutMe.addEventListener('click', () => dialogAboutMe.close());
+dialogAboutMeDiv.addEventListener('click', (event) => event.stopPropagation());
+
+const dialogMusicButton = document.getElementById('musicButton');
+const dialogMusic = document.getElementById('dialogMusic');
+const dialogMusicDiv = document.getElementById('musicStopPropagation');
+
+dialogMusicButton.addEventListener('click', () => dialogMusic.showModal());
+dialogMusic.addEventListener('click', () => dialogMusic.close());
+dialogMusicDiv.addEventListener('click', (event) => event.stopPropagation());
+
+const dialogHobbiesButton = document.getElementById('hobbiesButton');
+const dialogHobbies = document.getElementById('dialogHobbies');
+const dialogHobbiesDiv = document.getElementById('hobbiesStopPropagation');
+
+dialogHobbiesButton.addEventListener('click', () => dialogHobbies.showModal());
+dialogHobbies.addEventListener('click', () => dialogHobbies.close());
+dialogHobbiesDiv.addEventListener('click', (event) => event.stopPropagation());
+
+const dialogTop5Button = document.getElementById('top5Button');
+const dialogTop5 = document.getElementById('dialogTop5');
+const dialogTop5Div = document.getElementById('top5StopPropagation');
+
+dialogTop5Button.addEventListener('click', () => dialogTop5.showModal());
+dialogTop5.addEventListener('click', () => dialogTop5.close());
+dialogTop5Div.addEventListener('click', (event) => event.stopPropagation());
+
+const dialogBlogButton = document.getElementById('blogButton');
+const dialogBlog = document.getElementById('dialogBlog');
+const dialogBlogDiv = document.getElementById('blogStopPropagation');
+
+dialogBlogButton.addEventListener('click', () => dialogBlog.showModal());
+dialogBlog.addEventListener('click', () => dialogBlog.close());
+dialogBlogDiv.addEventListener('click', (event) => event.stopPropagation());
--- docs/js/fetch.js
+++ docs/js/fetch.js
@@ -1,13 +1,82 @@
+function getAge(dateOfBirth) {
+    var today = new Date();
+    var birthDate = new Date(dateOfBirth);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age
+}
+
+function dateOfBirthInString(year, month, day) {
+    yearString = year.toString();
+    monthString = month.toString();
+    dayString = day.toString();
+    string = yearString + '-' + monthString + '-' + dayString;
+    return string
+}
+
 // Haal uit de HTML het element dat bij de ID "domainName" hoort.
-const domainNameFile = document.getElementById("domainName");
+const domainNameVar = document.getElementById("domainName");
 // Haal uit de HTML het element dat bij de ID "name" hoort. 
-const nameFile = document.getElementById("name");
+const nameVar1 = document.getElementById("name1");
+const nameVar2 = document.getElementById("name2");
+
+const ageVar = document.getElementById("age")
+const cityVar = document.getElementById("city")
+const provinceVar = document.getElementById("province")
+const termVar = document.getElementById("term")
+const studyVar = document.getElementById("study")
+const minorVar = document.getElementById("minor")
+const jobCompanyVar = document.getElementById("jobCompany")
+const jobTitleVar = document.getElementById("jobTitle")
 // Haal uit de HTML het element dat bij de ID "trackName1" en "trackName2" hoort.
 const trackNameOne = document.getElementById("trackName1");
 const trackNameTwo = document.getElementById("trackName2");
 // Haal uit de HTML het element dat bij de ID "track1" en "track2" hoort.
 const trackOne = document.querySelector("#track1");
 const trackTwo = document.querySelector("#track2");
+
+const hero1name = document.getElementById("hero1");
+const hero1picture = document.querySelector("#hero1picture");
+const hero1context = document.getElementById("hero1context");
+
+const hero2name = document.getElementById("hero2");
+const hero2picture = document.querySelector("#hero2picture");
+const hero2context = document.getElementById("hero2context");
+
+const hero3name = document.getElementById("hero3");
+const hero3picture = document.querySelector("#hero3picture");
+const hero3context = document.getElementById("hero3context");
+
+const hero4name = document.getElementById("hero4");
+const hero4picture = document.querySelector("#hero4picture");
+const hero4context = document.getElementById("hero4context");
+
+const hero5name = document.getElementById("hero5");
+const hero5picture = document.querySelector("#hero5picture");
+const hero5context = document.getElementById("hero5context");
+
+const villain1name = document.getElementById("villain1");
+const villain1picture = document.querySelector("#villain1picture");
+const villain1context = document.getElementById("villain1context");
+
+const villain2name = document.getElementById("villain2");
+const villain2picture = document.querySelector("#villain2picture");
+const villain2context = document.getElementById("villain2context");
+
+const villain3name = document.getElementById("villain3");
+const villain3picture = document.querySelector("#villain3picture");
+const villain3context = document.getElementById("villain3context");
+
+const villain4name = document.getElementById("villain4");
+const villain4picture = document.querySelector("#villain4picture");
+const villain4context = document.getElementById("villain4context");
+
+const villain5name = document.getElementById("villain5");
+const villain5picture = document.querySelector("#villain5picture");
+const villain5context = document.getElementById("villain5context");
 
 // Variable waarin de locatie van een JSON file staat.
 const jsonFile = "./json/data.json"
@@ -26,9 +95,20 @@ const jsonFile = "./json/data.json"
         // Na de functie 'response', doe dan iets met de functie 'data'.
         .then((data) => {
             // Gebruik de variable 'domainNameFile' om in de HTML te zoeken naar de ID 'domainName' a.d.h.v. innerHTML, om vervolgens deze waarde gelijk te maken aan de waarde bij 'domainName' uit de JSON file.
-            domainNameFile.innerHTML = data.domainName;
+            domainNameVar.innerHTML = data.domainName;
             // Gebruik de variable 'nameFile' om in de HTML te zoeken naar de ID 'name' a.d.h.v. innerHTML, om vervolgens deze waarde gelijk te maken aan de waarde bij 'name' uit de JSON file.
-            nameFile.innerHTML = data.name;
+            nameVar1.innerHTML = data.name;
+            nameVar2.innerHTML = data.name;
+
+            ageVar.innerHTML = getAge(dateOfBirthInString(data.dateOfBirth[0].year,data.dateOfBirth[0].month,data.dateOfBirth[0].day));
+          
+            cityVar.innerHTML = data.city;
+            provinceVar.innerHTML = data.province;
+            termVar.innerHTML = data.term;
+            studyVar.innerHTML = data.study;
+            minorVar.innerHTML = data.minor;
+            jobCompanyVar.innerHTML = data.jobCompany;
+            jobTitleVar.innerHTML = data.jobTitle;
             // Gebruik de variable 'trackNameOne' om in de HTML te zoeken naar de ID 'trackNameOne' a.d.h.v. innerHTML, om vervolgens deze waarde gelijk te maken aan de waarde bij 'title' in de 1e array van tracks uit de JSON file.
             trackNameOne.innerHTML = data.tracks[0].title;
             // Gebruik de variable 'trackOne' om in de HTML te zoeken naar locatie van een audio bestand a.d.h.v. .src, om vervolgens deze waarde gelijk te maken aan de waarde bij 'location' in de 1e array van tracks uit de JSON file.
@@ -37,6 +117,46 @@ const jsonFile = "./json/data.json"
             trackNameTwo.innerHTML = data.tracks[1].title;
             // Gebruik de variable 'trackTwo' om in de HTML te zoeken naar locatie van een audio bestand a.d.h.v. .src, om vervolgens deze waarde gelijk te maken aan de waarde bij 'location' in de 2e array van tracks uit de JSON file.
             trackTwo.src = data.tracks[1].location;
+
+            hero1name.innerHTML = data.heroes[0].hero;
+            hero1picture.src = data.heroes[0].heropicture;
+            hero1context.innerHTML = data.heroes[0].herocontext;
+
+            hero2name.innerHTML = data.heroes[1].hero;
+            hero2picture.src = data.heroes[1].heropicture;
+            hero2context.innerHTML = data.heroes[1].herocontext;
+
+            hero3name.innerHTML = data.heroes[2].hero;
+            hero3picture.src = data.heroes[2].heropicture;
+            hero3context.innerHTML = data.heroes[2].herocontext;
+
+            hero4name.innerHTML = data.heroes[3].hero;
+            hero4picture.src = data.heroes[3].heropicture;
+            hero4context.innerHTML = data.heroes[3].herocontext;
+
+            hero5name.innerHTML = data.heroes[4].hero;
+            hero5picture.src = data.heroes[4].heropicture;
+            hero5context.innerHTML = data.heroes[4].herocontext;
+
+            villain1name.innerHTML = data.villains[0].villain;
+            villain1picture.src = data.villains[0].villainpicture;
+            villain1context.innerHTML = data.villains[0].villaincontext;
+
+            villain2name.innerHTML = data.villains[1].villain;
+            villain2picture.src = data.villains[1].villainpicture;
+            villain2context.innerHTML = data.villains[1].villaincontext;
+
+            villain3name.innerHTML = data.villains[2].villain;
+            villain3picture.src = data.villains[2].villainpicture;
+            villain3context.innerHTML = data.villains[2].villaincontext;
+
+            villain4name.innerHTML = data.villains[3].villain;
+            villain4picture.src = data.villains[3].villainpicture;
+            villain4context.innerHTML = data.villains[3].villaincontext;
+
+            villain5name.innerHTML = data.villains[4].villain;
+            villain5picture.src = data.villains[4].villainpicture;
+            villain5context.innerHTML = data.villains[4].villaincontext;
 
             // Genereer een console bericht met daarin de inhoud van de variable 'data', wat de gehele JSON file is.
             console.log(data);
